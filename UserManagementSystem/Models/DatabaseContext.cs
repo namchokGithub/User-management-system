526a0e54a4b7831f20e762bccf5151e828fb5b23
Namchok <pech4751@gmail.com>
2020-08-12 23:30:38 +0700

    add class model repository

---
+++ UserManagementSystem/Models/DatabaseContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace UserManagementSystem.Models
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        { }
+    }
+}
--- UserManagementSystem/Startup.cs
+++ UserManagementSystem/Startup.cs
@@ -50,7 +50,7 @@ namespace UserManagementSystem
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=Home}/{id?}");
             });
         }
     }
