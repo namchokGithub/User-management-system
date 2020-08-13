ed0ba44f4208514529f826f2043a01fd877e99bc
Namchok Singhachai <44488002+namchokGithub@users.noreply.github.com>
2020-08-13 13:55:54 +0700

    update repository

--- UserManagementSystem/Data/LogAccountRepository.cs
+++ UserManagementSystem/Data/LogAccountRepository.cs
@@ -3,6 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+/*
+ * Name: LogAccountRepository.cs
+ * Namespace: Data
+ * Author: Namchok
+ */
+
 namespace UserManagementSystem.Data
 {
     public class LogAccountRepository
