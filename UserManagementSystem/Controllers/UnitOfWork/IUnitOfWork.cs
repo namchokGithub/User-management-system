ae2d523565d4ce56ca9048534bc5f6cafe4e63f5
Namchok Singhachai <44488002+namchokGithub@users.noreply.github.com>
2020-08-13 13:43:15 +0700

    update unit of work

--- UserManagementSystem/Controllers/UnitOfWork/IUnitOfWork.cs
+++ UserManagementSystem/Controllers/UnitOfWork/IUnitOfWork.cs
@@ -3,6 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+/*
+ * Namespace: Controller/UnitOfWork
+ * Name: IUnitOfWork.cs
+ * Author: Namchok
+ */
+
 namespace UserManagementSystem.Controllers
 {
     public class IUnitOfWork
--- UserManagementSystem/Controllers/UnitOfWork/UnitOfWork.cs
+++ UserManagementSystem/Controllers/UnitOfWork/UnitOfWork.cs
@@ -3,6 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+/*
+ * Namespace: Controller/UnitOfWork
+ * Name: UnitOfWork.cs
+ * Author: Namchok
+ */
+
 namespace UserManagementSystem.Controllers
 {
     public class UnitOfWork
