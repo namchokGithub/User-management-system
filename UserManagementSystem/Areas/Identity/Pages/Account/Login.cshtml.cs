760c950ed1063000af0df23c4ec49bb5918db4f0
Wannapa Srijermtong <wannapaa_s@hotmail.com>
2020-08-19 16:33:10 +0700

    Edit view and add href in Login.cshtml and Delete Displayname in Login.cshtml.cs

--- UserManagementSystem/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ UserManagementSystem/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -45,15 +45,13 @@ namespace UserManagementSystem.Areas.Identity.Pages.Account
         {
             [Required]
             [EmailAddress]
-            [Display(Name = "Username")]
             public string Email { get; set; }
 
             [Required]
-            [Display(Name = "Password")]
             [DataType(DataType.Password)]
             public string Password { get; set; }
 
-            [Display(Name = "Remember me?")]
+            [Display(Name = "Remember me")]
             public bool RememberMe { get; set; }
         }
 
