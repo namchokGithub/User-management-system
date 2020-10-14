8b6d47a87a8aad423efad02bd6fc909a7ef9f43d
Namchok <pech4751@gmail.com>
2020-10-14 16:24:28 +0700

    update new directory

---
+++ User Management System/Models/ErrorViewModel.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace User_Management_System.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId
+        {
+            get; set;
+        }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
