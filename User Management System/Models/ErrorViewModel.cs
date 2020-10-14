d30ac4acd9f8a869e3ad4141fa3ed435d6f4ec29
Namchok <pech4751@gmail.com>
2020-10-14 17:19:08 +0700

    Add logs model

--- User Management System/Models/ErrorViewModel.cs
+++ User Management System/Models/ErrorViewModel.cs
@@ -1,14 +1,14 @@
-using System;
+/*
+ * Name: ErrorViewModel.cs
+ * Author: System
+ * Description: Model of Edit view model
+ */
 
 namespace User_Management_System.Models
 {
     public class ErrorViewModel
     {
-        public string RequestId
-        {
-            get; set;
-        }
-
+        public string RequestId { get; set; }
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
+    } // End ErrorViewModel
 }
