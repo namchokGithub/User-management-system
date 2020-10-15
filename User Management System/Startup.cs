05f50e55399e2ed2e93d3be26c59d8494f63564b
Namchok <pech4751@gmail.com>
2020-10-15 16:09:32 +0700

    Update program setting and startup setting

--- User Management System/Startup.cs
+++ User Management System/Startup.cs
@@ -1,35 +1,101 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using EmailService;
+using User_Management_System.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using User_Management_System.Controllers;
+using Microsoft.Extensions.DependencyInjection;
+
+/*
+ * Name : Startup
+ * Author: System
+ * Description: Setting up the project.
+ */
 
 namespace User_Management_System
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        /*
+         * Name: Startup
+         * Parameter: configuration(IConfiguration)
+         */
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
-        }
+        } // End Constructor
 
-        public IConfiguration Configuration
-        {
-            get;
-        }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
+        /*
+         * Name: ConfigureServices
+         * Parameter: services(IServiceCollection)
+         * Description: This method gets called by the runtime. Use this method to add services to the container.
+         */
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
-        }
+            // Repository
+            services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+            services.AddScoped<ILogsRepository, LogsRepository>();
+            services.AddScoped<IAccountRepository, AccountRepository>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+            // Cookies
+            services.ConfigureApplicationCookie(o =>
+            {
+                o.ExpireTimeSpan = TimeSpan.FromDays(14);
+                o.SlidingExpiration = true;
+                o.ReturnUrlParameter = "/";
+            });
+
+            // Service for send email
+            var emailConfig = Configuration
+                .GetSection("EmailConfiguration")
+                .Get<EmailConfiguration>();
+            services.AddSingleton(emailConfig);
+            services.AddScoped<IEmailSender, EmailSender>();
+
+            // Controller with view and add razor pages
+            services.AddControllersWithViews();
+            services.AddRazorPages();
+
+            // Add mvc
+            services.AddMvcCore().AddDataAnnotations();
+
+            // Service for Google authentication
+            services.AddAuthentication()
+            .AddGoogle(googleOptions =>
+            {
+                googleOptions.ClientId = "168669914421-am1vveumu5aaggtsnmceac4eedpp4nrb.apps.googleusercontent.com";
+                googleOptions.ClientSecret = "_Z33IY-6xE8B_DkbAUKWqTFX";
+                googleOptions.SignInScheme = IdentityConstants.ExternalScheme;
+            });
+
+            // Service for Facebook authentication
+            services.AddAuthentication()
+            .AddFacebook(facebookOptions =>
+            {
+                facebookOptions.AppId = "775622693211895";
+                facebookOptions.AppSecret = "bac3818b714dd5282277916f3c56f172";
+            });
+
+            // Service for Microsoft authentication
+            services.AddAuthentication()
+            .AddMicrosoftAccount(microsoftOptions =>
+            {
+                microsoftOptions.ClientId = "ed43a983-e56c-4a2a-a1e8-55b74d56fbc4";
+                microsoftOptions.ClientSecret = ".j-k5QvM40k4Mzm1d7PwWBAQv~w42_MV.e";
+            });
+        } // End ConfigureServices
+
+        /*
+         * Name: Configure
+         * Parameter: app(IApplicationBuilder), env(IWebHostEnvironment)
+         * Description: This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+         */
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -47,6 +113,7 @@ namespace User_Management_System
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
@@ -54,7 +121,8 @@ namespace User_Management_System
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
             });
-        }
-    }
+        } // End Configure
+    } // End Startup
 }
